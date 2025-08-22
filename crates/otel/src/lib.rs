e0aa5df375c8f1daffa33f0c6418099ed37fd280
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-08-22 19:24:36 +1200

    Support Wasmgrid (#1)
    * rename telemetry -> otel
    
    * tidy up initialization
    
    * comments

---
+++ crates/otel/src/lib.rs
@@ -0,0 +1,10 @@
+//! # Telemtry
+//!
+//! Telemetry is a module that provides functionality for collecting and
+//! reporting OpenTelemetry-based metrics.
+
+pub mod init;
+pub mod tracing;
+
+pub use init::Telemetry;
+pub use tracing::*;
