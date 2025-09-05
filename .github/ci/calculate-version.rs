3c52872229c9c8e1731edbaf2c09fcf9f1f865fa
andrew_weston <andrew.weston@propellerhead.co.nz>
2025-09-05 20:57:24 +1200

    semver check

--- .github/ci/calculate-version.rs
+++ .github/ci/calculate-version.rs
@@ -1,3 +1,34 @@
+extern crate cargo_semver_checks;
+
+use cargo_semver_checks::{ActualSemverUpdate, Check, GlobalConfig, ReleaseType, Rustdoc};
+use std::process::Command;
+
 fn main() {
     println!("Hello, world!");
+
+    let output = Command::new("git")
+        .arg("for-each-ref")
+        .arg("refs/remotes/origin")
+        .arg("--format")
+        .arg("%(refname)")
+        .output()
+        .unwrap();
+    assert!(output.status.success());
+
+    let current = Rustdoc::from_root("test_crates/trait_missing/old/");
+    let baseline = Rustdoc::from_root("test_crates/trait_missing/new/");
+    let mut config = GlobalConfig::new();
+    let mut check = Check::new(current);
+    let check = check.set_baseline(baseline);
+    let report = check.check_release(&mut config).unwrap();
+    assert!(!report.success());
+
+    let (_crate_name, crate_report) = report.crate_reports().iter().next().unwrap();
+    let required_bump = crate_report.required_bump().unwrap();
+    assert_eq!(required_bump, ReleaseType::Major);
+
+    // The "old" and "new" crates have the same version.
+    // The detected bump is the minimum-possible SemVer bump in a new release.
+    // Since the crates are v0.1.0, the minimum possible bump is minor.
+    assert_eq!(crate_report.detected_bump(), ActualSemverUpdate::Minor);
 }
